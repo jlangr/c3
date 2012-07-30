6e62d41040dfdb12ea4ba536226399b2672ed24d
jeff.langr <jlangr@jlangr-ubuntu2.(none)>
2012-07-30 10:34:47 -0600

    9

--- RetweetCollectionTest.cpp
+++ RetweetCollectionTest.cpp
@@ -4,10 +4,21 @@
 
 using namespace ::testing;
 
+// START:helper
 class ARetweetCollection: public Test {
 public:
    RetweetCollection collection;
+
+// START_HIGHLIGHT
+// TODO: google test might have a better way to do this...
+   void ASSERT_SIZE(unsigned int expected) {
+      ASSERT_THAT(collection.size(), Eq(expected));
+      ASSERT_THAT(collection.isEmpty(), Eq(0 == expected));
+   }
+// END_HIGHLIGHT
 };
+
+// END:helper
 
 TEST_F(ARetweetCollection, IsEmptyWhenCreated) {
    ASSERT_THAT(collection.isEmpty(), Eq(true));
@@ -29,21 +40,14 @@ TEST_F(ARetweetCollection, HasSizeOfOneAfterTweetAdded) {
    ASSERT_THAT(collection.size(), Eq(1));
 }
 
-// START:twoTests
+// START:helper
 TEST_F(ARetweetCollection, DecreasesSizeAfterRemovingTweet) {
    collection.add(Tweet());
    
    collection.remove(Tweet());
 
-   ASSERT_THAT(collection.size(), Eq(0));
+// START_HIGHLIGHT
+   ASSERT_SIZE(0);
+// END_HIGHLIGHT
 }
-
-// AVOID doing this
-TEST_F(ARetweetCollection, IsEmptyAfterRemovingTweet) {
-   collection.add(Tweet());
-   
-   collection.remove(Tweet());
-
-   ASSERT_THAT(collection.isEmpty(), Eq(true)); 
-}
-// END:twoTests
+// END:helper
