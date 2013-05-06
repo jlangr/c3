a134c522fa022752b2dd74fcab5193f90124f937
jlangr <jeff@langrsoft.com>
2013-05-06 15:54:54 -0600

    fix

--- RetweetCollectionTest.cpp
+++ RetweetCollectionTest.cpp
@@ -26,14 +26,14 @@ TEST_F(ARetweetCollection, HasSizeZeroWhenCreated) {
 TEST_F(ARetweetCollection, IsEmptyWhenItsSizeIsZero) {
    ASSERT_THAT(collection.size(), Eq(0));
 
-   ASSERT_THAT(collection.isEmpty(), Eq(true));
+   ASSERT_TRUE(collection.isEmpty());
 }
 
 TEST_F(ARetweetCollection, IsNotEmptyWhenItsSizeIsNonZero) {
    collection.add(Tweet());
    ASSERT_THAT(collection.size(), Gt(0));
 
-   ASSERT_THAT(collection.isEmpty(), Eq(false));
+   ASSERT_FALSE(collection.isEmpty());
 }
 
 // START:OneTweetFixture
@@ -56,7 +56,7 @@ public:
 
 // START:OneTweetTests
 TEST_F(ARetweetCollectionWithOneTweet, IsNoLongerEmpty) {
-   ASSERT_THAT(collection.isEmpty(), Eq(false));
+   ASSERT_FALSE(collection.isEmpty());
 }
 
 TEST_F(ARetweetCollectionWithOneTweet, HasSizeOfOne) {
