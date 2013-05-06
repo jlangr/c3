c482572b528790716ac78ea25870001dfccf134f
jlangr <jeff@langrsoft.com>
2013-05-06 15:49:03 -0600

    fix

--- RetweetCollectionTest.cpp
+++ RetweetCollectionTest.cpp
@@ -12,7 +12,7 @@ public:
 };
 
 TEST_F(ARetweetCollection, IsEmptyWhenCreated) {
-   ASSERT_THAT(collection.isEmpty(), Eq(true));
+   ASSERT_TRUE(collection.isEmpty());
 }
 
 TEST_F(ARetweetCollection, HasSizeZeroWhenCreated) {
